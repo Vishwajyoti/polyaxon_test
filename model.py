0f9d613682e34dcc5d6e4bd40d08ee4ba00667a3
vishwa <vishwajyoti.p@gmail.com>
2021-10-27 11:12:45 +0000

    Dumping directly in artifacts path

--- model.py
+++ model.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
     #Getting the artifacts path
     path_art = tracking.get_artifacts_path()
     print (path_art)
-    model_path=os.path.join(path_art+"modelbin/model.joblib")
+    model_path=os.path.join(path_art+"/modelbin/model.joblib")
     joblib.dump(rfr,model_path)
 
 
